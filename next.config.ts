66ca155ecff6e22d19292dd974d6c4e5b8229d9d
Natsuki Lin <linnatsuki221@gmail.com>
2025-03-29 02:04:52 +0800

    Update package.json and next.config.ts for deployment and production settings

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? "/Tailwind_css_uikit" : "",
+  assetPrefix: isProd ? "/Tailwind_css_uikit/" : "",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
     ],
   },
-  allowedDevOrigins: ['http://10.242.126.57:3000'],
 };
 
 export default nextConfig;
