5f4651fe259c74658a1f04ce53302de8c268e7ca
Natsuki Lin <linnatsuki221@gmail.com>
2025-03-29 15:24:40 +0800

    Remove production-specific configurations from next.config.ts and enable unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/Tailwind_css_uikit" : "",
-  assetPrefix: isProd ? "/Tailwind_css_uikit/" : "",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
